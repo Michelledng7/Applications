e8e3b20f9116a42cd60230d93c7b13bbc9b41c2b
Michelle <michelledng7@gmail.com>
2022-08-11 19:29:30 +1000

    add interface IState, JSX Element and React FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddList from './components/AddList';
+import List from './components/List';
+export interface IState {
+  people: {
+  name: string;
+  url: string;
+  age: number;
+  notes?: string;
+}[]
+}
 
 function App() {
+
+
+
+  const [people, setPeople] = useState<IState["people"]>([{
+    name: "Rafal Nadal",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSifvZdMo3sfz3nDJ7eCblT_yY9-WO4qCD1xQ&usqp=CAU",
+    age: 32,
+    notes: "The 22-time Grand Slam champion has always been appreciated for his ability to never give up"
+  }, 
+  {
+    name: "Roger Federer",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQriDlADuTpJp85y5HSV-KO2qAIhdgW39dHlQ&usqp=CAU",
+    age: 40,
+    notes: "Roger Federer holds several ATP records and is considered to be one of the greatest tennis players of all time."
+  },
+
+  {
+    name: "Marcos Giron",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfBaTA9YZFH2aUs-goMxRlRYAw9ch7PAkMVw&usqp=CAU",
+    age: 29,
+    notes: "Marcos Andres Giron (born July 24, 1993; Spanish: Marcos Girón) is an American professional tennis player."
+  },
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Tennis legendary players</h1>
+    <ul><List people = {people}/></ul>
+   <div><AddList people = {people} setPeople = {setPeople}/></div> 
     </div>
   );
 }
---
+++ src/components/AddList.tsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import {IState as Props} from '../App'
+
+interface IProps {
+    people: Props["people"]
+    setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+}
+
+// interface IPeople {
+//   people: {
+//   name: string;
+//   url: string;
+//   age: number;
+//   notes?: string;
+//   } []
+// }
+
+
+
+const AddList: React.FC<IProps> = ({people, setPeople}) => {
+
+  const [input, setInput] = useState({
+    name: "",
+    url: "",
+    age: "",
+    notes: ""
+  })
+
+const handleChanges = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>): void => {
+  setInput({
+    ...input, [e.target.name]: e.target.value
+})}
+
+const handleSubmit = () => {
+  if (!input.name || !input.url ) {
+    return
+  }
+ 
+  setPeople([...people, 
+   {  name : input.name,
+      url : input.url,
+      age : parseInt(input.age),
+      notes : input.notes
+   }])
+
+  setInput({
+    name: "",
+    url: "",
+    age: "",
+    notes: ""
+  })
+}
+
+
+  return (
+    <div className='AddToList'>
+      <input className = 'AddToList-input' type='text' placeholder = 'Name' value = {input.name} onChange ={handleChanges} name = 'name' />
+      <input className = 'AddToList-input' type='text' placeholder = 'Image URL' value = {input.url} onChange= {handleChanges} name ='url' />
+      <input className = 'AddToList-input' type='text' placeholder = 'Age' value = {input.age} onChange= {handleChanges} name ='age'/>
+      <textarea className = 'AddToList-input' placeholder = 'Notes' value = {input.notes} onChange = {handleChanges} name = 'notes' />     
+      <button className = 'AddToList-btn' onClick={handleSubmit} >Add to list</button>
+    </div>)
+    
+}
+
+export default AddList
